92c3c7e6927b310ddc777e62322c1f1595407145
Rin0059 <fandorin0059@gmail.com>
2021-06-04 07:11:39 +0000

    [Finish]5

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,33 @@ class User < ApplicationRecord
   def following?(user)
     self.followings.include?(user)
   end
+  
+  def self.search(search,word)
+    if search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @user = User.where("#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+  
+  
+  def self.search(search, word)
+    if search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @book = Book.where("#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
+      get 'search' => 'searches#search'
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
   end
+
   
   resources :books do
     resource :favorites, only: [:create, :destroy]
